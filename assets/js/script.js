07d58c44020342a7f1ac802d161ae077694798d0
Cravsi <shaun.bonner1@gmail.com>
2022-06-09 18:23:28 +0000

    [add] JS to display addition questions to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,30 @@ document.addEventListener('DOMContentLoaded', function() {
                 alert('You clicked Submit');
             } else {
                 let gameType = this.getAttribute('data-type');
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame('addition');
 })
 
-function runGame() {
+/**
+ * The main game "loop", called when the script is loaded
+ * and after the user's answer has been processed
+ */
+function runGame(gameType) {
+    
+    // Create two random number between 1 and 50
+    let num1 = Math.floor(Math.random()*50 + 1);
+    let num2 = Math.floor(Math.random()*50 + 1);
 
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`)
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -35,7 +51,14 @@ function incrementIncorrectAnswer () {
 
 }
 
-function displayAdditionQuestion () {
+/**
+ * Function to run 'addition questions'
+ */
+function displayAdditionQuestion (operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 
 }
 
