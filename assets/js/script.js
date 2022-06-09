ae6145ddf56dcda4812ef85ee0d5e1e4b53d4da5
Cravsi <shaun.bonner1@gmail.com>
2022-06-09 17:44:41 +0000

    [add] Setup function strucure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
-console.log("I'm Connected!");
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore () {
+
+}
+
+function incrementIncorrectAnswer () {
+
+}
+
+function displayAdditionQuestion () {
+
+}
+
+function displaySubtractionQuestion () {
+    
+}
+
+function displayMultiplyQuestion () {
+    
+}
+
+function displayDivideQuestion () {
+    
+}
